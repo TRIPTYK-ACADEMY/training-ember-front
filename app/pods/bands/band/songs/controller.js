d2c95d6e543ca6518f9e013153dc1489f899d36e
remadex <s.cardon@outlook.com>
2021-06-07 13:38:20 +0200

    exo correction

---
+++ app/pods/bands/band/songs/controller.js
@@ -0,0 +1,44 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { empty } from '@ember/object/computed';
+import { action } from '@ember/object';
+
+export default class SongsBandSongsController extends Controller {
+  @tracked isAddingSong = false;
+  @tracked selectSong = {
+    title: '',
+    rating: 0,
+  };
+
+  @empty('selectSong.title') isAddButtonDisabled;
+
+  @action
+  addingSong() {
+    this.isAddingSong = true;
+  }
+
+  @action
+  async addSong(e) {
+    e.preventDefault();
+    this.isAddingSong = false;
+    let record = this.store.createRecord('song', {
+      title: this.selectSong.title,
+      rating: this.selectSong.rating,
+      band: this.model,
+    });
+    await record.save();
+    this.selectSong = {
+      title: '',
+      rating: 0,
+    };
+  }
+
+  @action
+  cancelSong() {
+    this.isAddingSong = false;
+    this.selectSong = {
+      title: '',
+      rating: 0,
+    };
+  }
+}
