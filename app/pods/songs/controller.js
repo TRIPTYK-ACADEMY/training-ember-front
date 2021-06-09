35b2f7189b1805561e0c21c21119c69947deb100
remadex <s.cardon@outlook.com>
2021-06-09 15:25:17 +0200

    correction queryParams

--- app/pods/songs/controller.js
+++ app/pods/songs/controller.js
@@ -4,8 +4,10 @@ import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 
 export default class SongsController extends Controller {
-  queryParams = ['name'];
+  queryParams = ['name', 'rating'];
   @tracked name = 'asc'; // J'ai défini plus haut une queryParams (name qui est une propriété sur le controller) mais ici je définis une valeur par défaut à ma queryParams et je lui ajouté un décorateur tracked pour qu'elle soit suivi et qu'elle mette à jour le template quand elle est modifié
+
+  @tracked rating = 'asc';
 
   @service favoritesSong;
 
@@ -13,13 +15,35 @@ export default class SongsController extends Controller {
     return this.model.query == 'asc' ? 'desc' : 'asc';
   }
 
+  get directionRating() {
+    return this.model.queryRating == 'asc' ? 'desc' : 'asc';
+  }
+
   get filteredSong() {
     let songs = this.model.songs;
-    if (this.name === 'asc') {
-      return songs.sortBy('title');
-    } else {
-      return songs.sortBy('title').reverse();
+    console.log(
+      "queryParams 'name' : " + this.model.query + ' --- ' + this.name
+    );
+    console.log(
+      "queryParams 'rating' : " + this.model.queryRating + ' --- ' + this.rating
+    );
+
+    if (this.name) {
+      if (this.name === 'asc') {
+        return songs.sortBy('title');
+      } else {
+        return songs.sortBy('title').reverse();
+      }
     }
+    if (this.rating) {
+      if (this.rating === 'asc') {
+        return songs.sortBy('rating');
+      } else {
+        return songs.sortBy('rating').reverse();
+      }
+    }
+
+    return songs;
   }
 
   @action
--- app/pods/songs/route.js
+++ app/pods/songs/route.js
@@ -5,11 +5,15 @@ export default class SongsRoute extends Route {
     name: {
       refreshModel: true, // Cette ligne nous permet juste de dire que notre model doit se remettre à jour dès qu'on modifie une queryParams
     },
+    rating: {
+      refreshModel: true,
+    },
   };
   model(params) {
     const songs = this.store.findAll('song', { include: 'band' });
     return {
       query: params.name,
+      queryRating: params.rating,
       songs,
     };
   }
